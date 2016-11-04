164523ebbb71b450797c0effb2464e60dd9b05a9
AlOrozco53 <alorozco.patriot53@gmail.com>
2016-11-04 17:05:51 -0600

    [ADDED] all code and docs as seen TODAY in class

--- README.md
+++ README.md
@@ -5,15 +5,102 @@
 ***Facultad de Ciencias, UNAM***
 
 ## Ejecución
-En `bin/` se encuentra el archivo `btrees_hof.hs`, el cual se debe de interpretar
+En `bin/` se encuentra el archivo `btigeom.hs`, el cual se debe de interpretar
 desde dentro de dicha carpeta. Es decir,
 
 ```shell
 cd bin/
 
-ghci btrees_hof.hs
+ghci btigeom.hs
 ```
 
 ## Descripción
 
-TODO!!
+El código fuente está dividido en lo siguiente:
+
+### Listas
+
+Aquí solo se implementó la función `show` que hace instancia al tipo de datos `MyList a` a
+la clase `Show` (la cual permite que un tipo sea impreso en la terminal al convertirlo a
+`String`).
+
+- 
+  ```haskell
+  showList1 :: Show a => MyList a -> String
+  ```
+  Esta función es la que se revisó en clase e imprime listas en el estilo
+  `1 :(2 :(3 :[]))`.
+  
+- 
+  ```haskell
+  showList2 :: Show a => MyList a -> String
+  ```
+  Esta función imprime de *mejor* manera a una lista:
+  `[1,2,3]`. La estrategia que se usó fue, primero, separar el caso base e inmediatamente
+  evaluar a `[]`. Posteriormente, lo primero que se hace es concatenar el `[` con lo que
+  resulte de una función auxiliar y, finalmente, pegar el `]` al final. Dicha función auxiliar
+  coloca *comas* entre cada elemento de la lista (observe que el **primer** elemento de la lista
+  siempre se imprime fuera de la función auxiliar para no tener una coma delante de éste).
+
+Para visualizar lo que hace cada una de las funciones anteriores, basta cambiar el `showList2` a
+`showList1` en la declaración 
+```haskell
+instance Show a => Show (MyList a) where
+  show = showList2
+```
+y construir cualquier `MyList a` en la terminal:
+```haskell
+*BTInstGeom> Cons 1 (Cons 2 (Cons 3 Nil))
+```
+
+### Árboles binarios
+
+- 
+  ```haskell
+  heightBT :: BinTree a -> Int
+  ```
+  Nos brinda la altura de un árbol binario. Recordemos que, bajo nuestra definición, la altura de una
+  sola _hoja_ es 1 (ya que tiene dos hijos nulos) y la altura de un árbol `BTNil` es 0.
+  
+- 
+  ```haskell
+  insertBST :: (Ord a) =>  a -> BinTree a -> BinTree a
+  ```
+  Inserta un elemento a un árbol binario dado de acuerdo a un (árbol binario de búsqueda)[https://www.cs.usfca.edu/~galles/visualization/BST.html].
+  
+-
+  ```haskell
+  insertListBST :: (Ord a) => [a] -> BinTree a -> BinTree a
+  ```
+  Inserta todos los elementos de la lista dada en el árbol binario dado (BST). Ojo, esta función
+  es **un regalo**.
+  
+- 
+  ```haskell
+  inorder :: BinTree a -> [a]
+  ```
+  Realiza un recorrido `inorder`, es decir, regresa la lista *ordenada* de todos los elementos
+  en el árbol de búsqueda binaria **dado**. Recordemos que el recorrido `inorder` recorre
+  recursivamente, primero, el hijo izquierdo de una rama luego va hacia la raíz y, finalmente,
+  recorre recursivamente el hijo derecho.
+
+### Geometría
+
+- 
+  ```haskell
+  makeTriangles :: [Coord] -> [Coord] -> [Coord] -> [Polygon]
+  ```
+  Solución elegante al problema dejado en la práctica 5.
+  
+- 
+  ```haskell
+  makeTriangles' :: [Coord] -> [Coord] -> [Coord] -> [Polygon]
+  ```
+  Una *pseudo-*solución al problema, inspirado por algún alumno de la clase. Obsérvese que
+  en este caso se producen muchos menos triángulos de los que se deberían de producir.
+  Sin embargo, como **no** es trivial llegar a la solución elegante, se considerará positivo
+  haber puesto una solución como ésta en la práctica
+  
+Con respecto a la función `isTriangle`, se explicará a detalle la implementación cuando
+se suba la solución a otra rama (REMINDER: put link here).
+
---
+++ bin/btigeom.hs
@@ -0,0 +1,119 @@
+{- |
+Module      :  BTInstGeom
+Maintainer  :  alorozco.patriot53@gmail.com
+Author      :  AlOrozco53
+Stability   :  experimental
+Portability :  portable
+Version     :  1.1
+
+Some examples for the 2017-1 Discrete Structures course.
+Taught at Facultad de Ciencias, UNAM.
+Topics mainly include binary trees, instance declarations,
+and geometry
+-}
+
+module BTInstGeom where
+
+-- My List implementation
+
+data MyList a = Nil | Cons a (MyList a)
+
+-- allow MyList to be printed
+instance Show a => Show (MyList a) where
+  show = showList2
+
+-- a "pretty printing" show function
+showList1 :: Show a => MyList a -> String
+showList1 Nil = "[]"
+showList1 (Cons x xs) = show x ++ " :(" ++ showList1 xs ++ ")"
+
+-- a more "pretty printing" show function
+showList2 :: Show a => MyList a -> String
+showList2 Nil = "[]"
+showList2 (Cons x xs) = "[" ++ show x ++ showAux xs ++ "]"
+  where
+    showAux Nil = ""
+    showAux (Cons y ys) = "," ++ show y ++ showAux ys
+
+
+-- Binary trees
+
+-- Binary Tree type declaration
+data BinTree a = BTNil
+               | BTBranch a (BinTree a) (BinTree a)
+               deriving Eq
+
+-- make a binary tree instance of the Show class
+instance Show a => Show (BinTree a) where
+  show btree = showAux btree ""
+    where
+      showAux BTNil acc = "Nil"
+      showAux (BTBranch r lchild rchild) acc = let newacc = acc ++ "\t"
+                                               in "Branch " ++ show r ++
+                                                  "\n" ++ newacc ++ showAux rchild newacc ++
+                                                  "\n" ++ newacc ++ showAux lchild newacc
+
+-- Gives the height of a binary tree
+heightBT :: BinTree a -> Int
+heightBT BTNil = 0
+heightBT (BTBranch x lchild rchild) = 1 + max (heightBT lchild) (heightBT rchild)
+
+-- Insert an element in a "binary search tree" fashion
+insertBST :: (Ord a) =>  a -> BinTree a -> BinTree a
+insertBST x BTNil = BTBranch x BTNil BTNil
+insertBST x (BTBranch r lchild rchild)
+  | x <= r = BTBranch r (insertBST x lchild) rchild
+  | otherwise = BTBranch r lchild (insertBST x rchild)
+
+-- Given a list l of elements that can be ordered and a binary tree t,
+-- inserts all the elements of l in t in a BST fashion.
+insertListBST :: (Ord a) => [a] -> BinTree a -> BinTree a
+insertListBST list btree = foldr insertBST btree (reverse list)
+
+-- BST inorder traversal
+inorder :: BinTree a -> [a]
+inorder BTNil = []
+inorder (BTBranch r lchild rchild) = inorder lchild ++ [r] ++ inorder rchild
+
+
+-- Geometry
+
+type Coord = (Double, Double)
+
+type LineSegment = (Coord, Coord)
+
+data Polygon = Triangle Coord Coord Coord
+             | Rectangle Coord Coord Coord Coord
+             | ConvexPolygon [LineSegment]
+             deriving (Show, Eq)
+
+-- Decides if a Polygon is a (valid) triangle
+isTriangle :: Polygon -> Bool
+isTriangle (Triangle a b c)
+  | fst a == fst b = False
+  | fst b == fst c = False
+  | fst c == fst a = False
+  | otherwise = not $ (m a b) == (m a c)
+  where
+    m s t = ((snd s) - (snd t)) / ((fst s) - (fst t))
+isTriangle (ConvexPolygon lineList) = if length lineList == 3
+                                      then isTriangle $ Triangle v1 v2 v3
+                                      else False
+  where
+    v1 = fst $ lineList !! 1
+    v2 = fst $ lineList !! 2
+    v3 = fst $ lineList !! 3
+
+-- Makes all the possible triangles with the ith vertex taken from the
+-- ith argument
+makeTriangles :: [Coord] -> [Coord] -> [Coord] -> [Polygon]
+makeTriangles xs ys zs = [Triangle x y z | x <- xs, y <- ys, z <- zs, isTriangle $ Triangle x y z]
+
+-- Another (pseudo-)implementation for the previous function
+makeTriangles' :: [Coord] -> [Coord] -> [Coord] -> [Polygon]
+makeTriangles' [] _ _ = []
+makeTriangles' _ [] _ = []
+makeTriangles' _ _ [] = []
+makeTriangles' (x:xs) (y:ys) (z:zs) = if isTriangle $ Triangle x y z
+                                      then makeTriangles' xs ys zs ++ [Triangle x y z]
+                                      else makeTriangles' xs ys zs
