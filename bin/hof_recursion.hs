178f28d5f0e3d9d0a76a457d8b94caff43160304
AlOrozco53 <alorozco.patriot53@gmail.com>
2016-11-11 08:22:05 -0600

    [FIXED] a factorial bug

--- bin/hof_recursion.hs
+++ bin/hof_recursion.hs
@@ -18,10 +18,11 @@ import PicturesSVG
 -- Recursion
 
 -- Factorial
-fact :: Int -> Int
+fact :: (Ord a, Num a) => a -> a
 fact n
   | n <= 0 = 1
   | otherwise = n * (fact $ n-1)
+
 
 -- Pictures
 
